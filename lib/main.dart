21ee59e353d6a69f4f71a9bf0c0e5f1365412e5f
Andrehlb <andreh.72@hotmail.com>
2025-08-05 19:46:43 -0300

    Add .env to .gitignore and import Supabase Flutter package in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:app4_receitas/routes/app_router.dart';
 import 'package:app4_receitas/utils/theme/custom_theme_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
   // Garante que o Flutter está inicializado
