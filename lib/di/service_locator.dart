7ca2ff50cdd9b9f30548c2b27a6d64421396c19d
Andrehlb <andreh.72@hotmail.com>
2025-08-06 06:34:28 -0300

    Change Recipe ViewModel registration from factory to lazy singleton in service locator

--- lib/di/service_locator.dart
+++ lib/di/service_locator.dart
@@ -17,5 +17,5 @@ import 'package:app4_receitas/ui/recipes/recipes_viewmodel.dart';
  getIt.registerLazySingleton<RecipeRepository>(() => RecipeRepository());
 
   // Recipe ViewModel
-  getIt.registerFactory<RecipesViewModel>(() => RecipesViewModel());
+  getIt.registerLazySingleton<RecipesViewModel>(() => RecipesViewModel());
 }
