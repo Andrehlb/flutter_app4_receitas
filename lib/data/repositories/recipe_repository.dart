345d3d494ee08caba8b1e2360fcef54a42271c93
Andrehlb <andreh.72@hotmail.com>
2025-08-10 13:50:54 -0300

    Refactor getRecipes method to remove debug print statements and improve error handling

--- lib/data/repositories/recipe_repository.dart
+++ lib/data/repositories/recipe_repository.dart
@@ -6,22 +6,13 @@ class RecipeRepository {
   final _service = getIt<RecipeService>();
   
   Future<List<Recipe>> getRecipes() async {
-    print('getRecipes chamado!'); // Debug print statement
-
+    // print('getRecipes chamado!'); // Debug print statement
     try {
       final rawData = await _service.fetchRecipes();
-      print('Dados rawData recebidos: $rawData (${rawData.runtimeType})'); // Debug print statement
-
-      if (rawData == null) return []; // Garatir lista vazia se null
-
-      // Garatir lista vazia se null
-      if (rawData is! List){
-        throw Exception('Dados Recebidos não são uma lista: $rawData');
-      }
-
-      return rawData.map((data) => Recipe.fromJson(data)).toList();
+      // print('Dados rawData recebidos: $rawData (${rawData.runtimeType})'); // Debug print statement
+      return rawData.map((sata) => Recipe.fromJson(data)).toList();
     } catch (e) {
-      throw Exception('Falhou ao buscar receitas: $e');
+      throw Exception('Hum! Falhou ao buscar receitas 😬: ${e.toString()}');
     }
   }
 }
