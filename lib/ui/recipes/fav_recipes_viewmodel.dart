c41ddcebc63f73684c60e125f177b350a0bdedc0
Andrehlb <andreh.72@hotmail.com>
2025-08-10 16:52:07 -0300

    Add state management properties and getters to FavRecipesViewModel

--- lib/ui/recipes/fav_recipes_viewmodel.dart
+++ lib/ui/recipes/fav_recipes_viewmodel.dart
@@ -6,3 +6,11 @@ import 'package:app4_receitas/di/service_locator.dart';
 class FavRecipesViewModel extends GetxController {
   final _repository = getIt<RecipeRepository>();
 
+  final RxList<Recipe> _favRecipes = <Recipe>[].obs;
+  final RxBool _isLoading = false.obs;
+  final RxString _errorMessage = ''.obs;
+
+  // Getters
+  List<Recipe> get favRecipes => _favRecipes;
+  bool get isLoading => _isLoading.value;
+  String? get errorMessage => _errorMessage.value;
