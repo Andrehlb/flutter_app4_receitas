993f9560d2a1f16c819e390e07de20f135eb62f7
Andrehlb <andreh.72@hotmail.com>
2025-08-14 07:09:40 -0300

    Add import for UserProfile in AuthService

--- lib/data/services/auth_service.dart
+++ lib/data/services/auth_service.dart
@@ -1,4 +1,5 @@
 import 'package:app4_receitas/di/service_locator.dart';
+import 'package:app4_receitas/data/models/user_profile.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AuthService {
