0f73ff5bed5c2f753c9da9d1d476fb31aea96062
Andrehlb <andreh.72@hotmail.com>
2025-08-21 21:10:43 -0300

    Fix import statement order in AuthService for consistency

--- lib/data/services/auth_service.dart
+++ lib/data/services/auth_service.dart
@@ -1,8 +1,8 @@
 // import 'package:dartz/dartz.dart';
-// import 'package:app4_receitas/di/service_locator.dart';
 // import 'package:app4_receitas/data/models/user_profile.dart';
 import 'package:either_dart/either.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:app4_receitas/di/service_locator.dart';
 import 'package:app4_receitas/utils/app_error.dart';
 
 class AuthService {
