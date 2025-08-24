cb3d1e44af58c874bba4ac2ebe707dcff37e4cfb
Andrehlb <andreh.72@hotmail.com>
2025-08-23 21:48:14 -0300

    12-08_A13-feat(Auth and FavRecipes): Add AuthView widget with initial state management

---
+++ lib/ui/auth/auth_view.dart
@@ -0,0 +1,12 @@
+import 'package:app4_receitas/di/service_locator.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'auth_viewmodel.dart';
+
+class AuthView extends StatefulWidget {
+  const AuthView({super.key});
+
+  @override
+  State<AuthView> createState() => _AuthViewState();
+}
