763aea706886c60bc69c436c7c4b41a2fd7ccbcf
Andrehlb <andreh.72@hotmail.com>
2025-08-26 05:14:23 -0300

    fix14-08-A14-ProfileView, CustomBNB, signOut-(app_router.dart): remove unused imports for cleaner code

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -1,5 +1,5 @@
-// import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:app4_receitas/data/services/auth_service.dart';
+import 'package:app4_receitas/di/service_locator.dart';
 // import 'package:app4_receitas/ui/auth/auth_view.dart';
 import 'package:app4_receitas/ui/recipes/recipes_view.dart';
 // import 'package:app4_receitas/ui/recipes/fav_recipes_view.dart';
