11064d69c38511c43e4f2c77f63571b83b6b26a8
Andrehlb <andreh.72@hotmail.com>
2025-08-26 06:53:22 -0300

    12-08-A13-Auth and FavRecipes-fix(RecipeDetailView): add missing closing parenthesis in widget tree

--- lib/ui/recipedetail/recipe_detail_view.dart
+++ lib/ui/recipedetail/recipe_detail_view.dart
@@ -194,4 +194,4 @@ class RecipeDetailView extends StatefulWidget {
                               );
                             }),
                           ],
-                          
+                        ),
