0bc5cc765bc46830f7b4478b6b5fcb9b50f12dc6
Andrehlb <andreh.72@hotmail.com>
2025-08-26 06:26:06 -0300

    fix14-08-A14-ProfileView, CustomBNB, signOut-fix: remove unused import for Supabase in FavRecipesViewModel

--- lib/ui/fav_recipes/fav_recipes_viewmodel.dart
+++ lib/ui/fav_recipes/fav_recipes_viewmodel.dart
@@ -1,7 +1,7 @@
 import 'package:get/get.dart';
 import 'package:either_dart/either.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:app4_receitas/data/models/recipe.dart';
+import 'package:app4_receitas/data/repositories/auth_repository.dart';
 import 'package:app4_receitas/data/repositories/recipe_repository.dart';
 import 'package:app4_receitas/di/service_locator.dart';
 
