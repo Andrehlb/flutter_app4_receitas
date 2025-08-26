a9d2a88c0b09f49b51953467251cd5a670946755
Andrehlb <andreh.72@hotmail.com>
2025-08-26 05:19:46 -0300

    fix14-08-A14-ProfileView, CustomBNB, signOut-fix(app_router.dart): update import for profile_view and remove duplicate base_screen import

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -3,7 +3,7 @@ import 'package:app4_receitas/di/service_locator.dart';
 import 'package:app4_receitas/ui/auth/auth_view.dart';
 import 'package:app4_receitas/ui/base_screen.dart';
 import 'package:app4_receitas/ui/fav_recipes/fav_recipes_view.dart';
-import 'package:app4_receitas/ui/base_screen.dart';
+import 'package:app4_receitas/ui/profile/profile_view.dart';
 
 class AppRouter {
 import 'package:go_router/go_router.dart';
