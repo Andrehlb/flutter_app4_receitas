c59396b6689b29910fc9fec9bd4b6b89f11bf5a4
Andrehlb <andreh.72@hotmail.com>
2025-08-26 06:51:17 -0300

    19-08_A15-SignUp, Animations-refactor(RecipeDetailView): update error handling and navigation in the build method

--- lib/ui/recipedetail/recipe_detail_view.dart
+++ lib/ui/recipedetail/recipe_detail_view.dart
@@ -44,159 +44,153 @@ class RecipeDetailView extends StatefulWidget {
     });
   }
 
-      if (viewModel.errorMessage != '') {
-        return Center(
-          child: Container(
-            padding: EdgeInsets.all(32),
-            child: Column(
-              spacing: 32,
-              children: [
-                Text(
-                  'Erro: ${viewModel.errorMessage}',
-                  style: TextStyle(fontSize: 24),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    viewModel.getRecipes();
-                  },
-                  child: Text('TENTAR NOVAMENTE'),
-                ),
-              ],
-            ),
-          ),
-        );
-      }
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
-      return Container(
-        padding: EdgeInsets.all(16),
-        child: Column(
+  @override
+  Widget build(BuildContext context) {
+    return Builder(
+      builder: (context) {
+        if (viewModel.isLoading) {
+          return Center(
+            child: SizedBox(
+              height: 96,
+              width: 96,
+              child: CircularProgressIndicator(strokeWidth: 12),
+            ),
+          );
+        }
+
+        if (viewModel.errorMessage! != '') {
+          return Center(
+            child: Container(
+              padding: EdgeInsets.all(32),
+              child: Column(
+                spacing: 32,
+                children: [
+                  Text(
+                    'Erro: ${viewModel.errorMessage}',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      context.go('/');
+                    },
+                    child: Text('VOLTAR'),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+
+        final recipe = viewModel.recipe;
+        return Stack(
           children: [
-            Expanded(
-              child:
-              viewModel.recipes.isNotEmpty
-                  ? Center(
-                      child: Column(
-                        children: [
-                          Text(
-                            '${viewModel.recipes.length} receitas(s)',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  Image.network(
+                    recipe!.image!,
+                    height: 400,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                    loadingBuilder: (context, child, loadingProgress) =>
+                        loadingProgress == null
+                        ? child
+                        : Center(
+                            child: CircularProgressIndicator(
+                              color: Theme.of(context).colorScheme.primary,
                             ),
                           ),
-                          const SizedBox(height: 16),
-                          Expanded(
-                            child: ListView.builder(
-                              itemCount: viewModel.recipes.length,
-                              itemBuilder: (context, index) {
-                                final recipe = viewModel.recipes[index];
-                                // final isFavorite = viewModel.favRecipes.any(
-                                //   (fav) => fav.id == recipe.id,
-                                // );
-                                return Stack(
-                                  children: [
-                                    GestureDetector(
-                                      onTap: () =>
-                                          context.go('/recipe/${recipe.id}'),
-                                      child: RecipeCard(recipe: recipe),
-                                    ),
-                                    // Positioned(
-                                    //   top: 16,
-                                    //   right: 16,
-                                    //   child: IconButton(
-                                    //     icon: Icon(
-                                    //       isFavorite
-                                    //           ? Icons.favorite
-                                    //           : Icons.favorite_border,
-                                    //       size: 32,
-                                    //       color: isFavorite ? Colors.red : null,
-                                    //     ),
-                                    //     onPressed: () {
-                                    //       if (isFavorite) {
-                                    //         viewModel.removeFromFavorites(
-                                    //           recipe,
-                                    //         );
-                                    //         if (context.mounted) {
-                                    //           ScaffoldMessenger.of(
-                                    //             context,
-                                    //           ).clearSnackBars();
-                                    //           ScaffoldMessenger.of(
-                                    //             context,
-                                    //           ).showSnackBar(
-                                    //             SnackBar(
-                                    //               content: Text(
-                                    //                 '${recipe.name} desfavoritada!',
-                                    //               ),
-                                    //               duration: Duration(
-                                    //                 seconds: 3,
-                                    //               ),
-                                    //               action: SnackBarAction(
-                                    //                 label: 'DESFAZER',
-                                    //                 onPressed: () {
-                                    //                   viewModel.addToFavorites(
-                                    //                     recipe,
-                                    //                   );
-                                    //                 },
-                                    //               ),
-                                    //             ),
-                                    //           );
-                                    //         }
-                                    //       } else {
-                                    //         viewModel.addToFavorites(recipe);
-                                    //         if (context.mounted) {
-                                    //           ScaffoldMessenger.of(
-                                    //             context,
-                                    //           ).clearSnackBars();
-                                    //           ScaffoldMessenger.of(
-                                    //             context,
-                                    //           ).showSnackBar(
-                                    //             SnackBar(
-                                    //               content: Text(
-                                    //                 '${recipe.name} favoritada!',
-                                    //               ),
-                                    //               duration: const Duration(
-                                    //                 seconds: 2,
-                                    //               ),
-                                    //             ),
-                                    //           );
-                                    //         }
-                                    //       }
-                                    //     },
-                                    //   ),
-                                    // ),
-                                  ],
-                                );
-                              },
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
-                  :
-                  Center(
-                      child: Column(
-                        children: [
-                          const SizedBox(height: 64),
-                          Icon(
-                            Icons.favorite,
-                            size: 96,
+                    errorBuilder: (context, child, stackTrace) => Container(
+                      height: 400,
+                      width: double.infinity,
+                      color: Theme.of(context).colorScheme.primary,
+                      child: Icon(Icons.error),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      children: [
+                        Text(
+                          recipe.name,
+                          style: GoogleFonts.dancingScript(
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
                             color: Theme.of(context).colorScheme.primary,
                           ),
-                          const SizedBox(height: 32),
-                          Text(
-                            'Adicione suas receitas favoritas!',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18,
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 16),
+                        RecipeRowDetails(recipe: recipe),
+                        const SizedBox(height: 16),
+                        recipe.ingredients.isNotEmpty
+                            ? Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: [
+                                  Text(
+                                    'Ingredientes:',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 8),
+                                  Text(recipe.ingredients.join('\n')),
+                                ],
+                              )
+                            : Text('Nenhum ingrediente listado.'),
+                        const SizedBox(height: 16),
+                        recipe.instructions.isNotEmpty
+                            ? Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: [
+                                  Text(
+                                    'Instruções:',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 8),
+                                  Text(recipe.instructions.join('\n')),
+                                ],
+                              )
+                            : Text('Nenhuma instrução :('),
+                        const SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            ElevatedButton(
+                              onPressed: () => context.go('/'),
+                              child: Text('VOLTAR'),
                             ),
-                          ),
-                        ],
-                      ),
-                    ),
-            ),
-          ],
-        ),
-      );
-    });
-  }
-}
+                            Obx(() {
+                              return AnimatedSwitcher(
+                                duration: const Duration(milliseconds: 300),
+                                transitionBuilder: (child, animation) {
+                                  return ScaleTransition(
+                                    scale: animation,
+                                    child: child,
+                                  );
+                                },
+                                child: ElevatedButton(
+                                  key: ValueKey<bool>(viewModel.isFavorite),
+                                  onPressed: () {
+                                    viewModel.toggleFavorite();
+                                    _animationController.forward();
+                                  },
+                                  child: Text(
+                                    viewModel.isFavorite
+                                        ? 'DESFAVORITAR'
+                                        : 'FAVORITAR',
+                                  ),
+                                ),
+                              );
+                            }),
+                          
