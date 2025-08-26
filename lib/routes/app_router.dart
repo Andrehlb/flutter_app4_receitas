a0bc0d414829ba707e0f9dec4ee5202a4502d47e
Andrehlb <andreh.72@hotmail.com>
2025-08-26 05:17:29 -0300

    07-08-A12-RecipeDetail, Profiles and Favorites tables-fix: update imports in app_router.dart for correct routing and cleaner code

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -1,8 +1,7 @@
 import 'package:app4_receitas/data/services/auth_service.dart';
 import 'package:app4_receitas/di/service_locator.dart';
-// import 'package:app4_receitas/ui/auth/auth_view.dart';
-import 'package:app4_receitas/ui/recipes/recipes_view.dart';
-// import 'package:app4_receitas/ui/recipes/fav_recipes_view.dart';
+import 'package:app4_receitas/ui/auth/auth_view.dart';
+import 'package:app4_receitas/ui/base_screen.dart';
 // import 'package:app4_receitas/ui/recipedetail/recipe_detail_view.dart';
 import 'package:app4_receitas/ui/base_screen.dart';
 
