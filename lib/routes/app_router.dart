cff0dcad08de90c29f1206b330f59a8a6bd24ecb
Andrehlb <andreh.72@hotmail.com>
2025-08-26 05:18:34 -0300

    12-08-A13-Auth and FavRecipes-fix(app_router.dart): update import for fav_recipes_view and remove commented recipe_detail_view import

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -2,7 +2,7 @@ import 'package:app4_receitas/data/services/auth_service.dart';
 import 'package:app4_receitas/di/service_locator.dart';
 import 'package:app4_receitas/ui/auth/auth_view.dart';
 import 'package:app4_receitas/ui/base_screen.dart';
-// import 'package:app4_receitas/ui/recipedetail/recipe_detail_view.dart';
+import 'package:app4_receitas/ui/fav_recipes/fav_recipes_view.dart';
 import 'package:app4_receitas/ui/base_screen.dart';
 
 class AppRouter {
