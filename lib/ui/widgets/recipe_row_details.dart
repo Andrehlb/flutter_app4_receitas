ec26343165f33a3e09df069b2c9dc77bd6c1f2c3
Andrehlb <andreh.72@hotmail.com>
2025-08-26 07:26:33 -0300

    07-08-A12-RecipeDetail, Profiles and Favorites tables-refactor(RecipeRowDetails): enhance layout and display recipe details with icons

--- lib/ui/widgets/recipe_row_details.dart
+++ lib/ui/widgets/recipe_row_details.dart
@@ -1,24 +1,94 @@
 import 'package:flutter/material.dart';
-
+import 'package:app4_receitas/data/models/recipe.dart';
 class RecipeRowDetails extends StatelessWidget {
-  const RecipeRowDetails({super.key, required this.title, required this.value});
-  final String title;
-  final String value;
+  const RecipeRowDetails({
+    super.key, 
+    required this.recipe,
+  });
+
+  final Recipe recipe;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 4),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            '$title: ',
-            style: const TextStyle(fontWeight: FontWeight.bold),
-          ),
-          Expanded(child: Text(value)),
-        ],
-      ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Column(
+          children: [
+            Icon(
+              Icons.help_outline_sharp,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            Text(
+              '${recipe.difficulty}',
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            Icon(
+              Icons.watch_later_outlined,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            Text(
+              '${recipe.prepTimeMinutes} min',
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            Icon(
+              Icons.people_alt_outlined,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            Text(
+              '${recipe.servings}',
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            Icon(
+              Icons.local_fire_department_outlined,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            Text(
+              '${recipe.caloriesPerServing} cal',
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            Icon(
+              Icons.star_border_outlined,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            Text(
+              '${recipe.rating?.toStringAsFixed(1) ?? 'N/A'} (${recipe.reviewCount ?? 0})',
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
